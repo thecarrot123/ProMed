01644cade762eca1dad4fe40d30a0c0f1d6d4e16
thecarrot123 <theknife2001.al@gmail.com>
2022-03-07 09:20:47 +0200

    fix alharamtransfer

--- main2/serializers.py
+++ main2/serializers.py
@@ -3,11 +3,6 @@ from main2.models import Library, Transfer
 
 class TransferFromAlharamSerializer(serializers.ModelSerializer):
     #image = Base64ImageField(max_length=None,represent_in_base64 = True)
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        if(Transfer.objects.filter(receipt_number = data['receipt_number']).exists()):
-            raise serializers.ValidationError(".يجب ان يكون رقم الايصال فريد. الرجاء التاكد من صحة المعلومات")
-        return data
     class Meta:
         model = Transfer
         fields = ['user','receipt_number','amount']
