3b39a777f5b8a42ee60dc76ef4538c967ef84a63
thecarrot123 <theknife2001.al@gmail.com>
2022-03-07 09:49:02 +0200

    fix alharamtransfer

--- main2/serializers.py
+++ main2/serializers.py
@@ -6,12 +6,20 @@ class TransferFromAlharamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
         fields = ['user','receipt_number','amount']
+        extra_kwargs = {
+            'receipt_number': {'required': True},
+            'amount': {'required': True},
+        }
 
 class TransferFromLibrarySerializer(serializers.ModelSerializer):
     #image = Base64ImageField(max_length=None,represent_in_base64 = True)
     class Meta:
         model = Transfer
         fields = ['user','library','amount']
+        extra_kwargs = {
+            'library': {'required': True},
+            'amount': {'required': True},
+        }
 
 class LibrarySerializer(serializers.ModelSerializer):
     class Meta:
