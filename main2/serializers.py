aef018dbfe3d7d711741d9fccad18bb9e059b0a6
thecarrot123 <theknife2001.al@gmail.com>
2022-03-07 09:46:01 +0200

    fix alharamtransfer

--- main2/serializers.py
+++ main2/serializers.py
@@ -3,9 +3,6 @@ from main2.models import Library, Transfer
 
 class TransferFromAlharamSerializer(serializers.ModelSerializer):
     #image = Base64ImageField(max_length=None,represent_in_base64 = True)
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        return data
     class Meta:
         model = Transfer
         fields = ['user','receipt_number','amount']
