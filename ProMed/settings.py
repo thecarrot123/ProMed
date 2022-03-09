d840e1629743d9a39dd0a2afdd8a83c203c30067
thecarrot123 <theknife2001.al@gmail.com>
2022-03-09 15:34:58 +0200

    fix sending email 3

--- ProMed/settings.py
+++ ProMed/settings.py
@@ -145,7 +145,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
