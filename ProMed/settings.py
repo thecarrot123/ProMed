746410362493aa2bddcc0e601e2f5e7314f02026
thecarrot123 <theknife2001.al@gmail.com>
2022-03-09 21:50:39 +0200

    fix sending email 3

--- ProMed/settings.py
+++ ProMed/settings.py
@@ -145,11 +145,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alawirmalek@gmail.com'
-EMAIL_HOST_PASSWORD = 'qexvfbmqootrsluk'
+EMAIL_HOST_PASSWORD = '#AChEPT4Ang2%'
+#EMAIL_HOST_PASSWORD = 'qexvfbmqootrsluk'
 
 from os.path import expanduser
 home = expanduser("~")
