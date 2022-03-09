4029c9c0dc71a50a81d59da821e4a3434bb9c843
thecarrot123 <theknife2001.al@gmail.com>
2022-03-09 22:04:44 +0200

    fix sending email 3

--- ProMed/settings.py
+++ ProMed/settings.py
@@ -149,8 +149,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alawirmalek@gmail.com'
-EMAIL_HOST_PASSWORD = '#AChEPT4Ang2%'
-#EMAIL_HOST_PASSWORD = 'qexvfbmqootrsluk'
+EMAIL_HOST_PASSWORD = 'qexvfbmqootrsluk'
 
 from os.path import expanduser
 home = expanduser("~")
--- main/utils.py
+++ main/utils.py
@@ -1,20 +1,22 @@
-from ProMed.settings import EMAIL_HOST_USER
+from django.core.mail import EmailMessage
 from main.models import User
-from django.core.mail import send_mail
 import string,secrets
 
 class Util:
     @staticmethod
     def send_email(data):
-        send_mail(subject=data['subject'],message=data['body'],recipient_list=[data['email'],],from_email=EMAIL_HOST_USER)
+        email = EmailMessage(subject=data['subject'],body=data['body'],to=[data['email'],])
+        email.send()
 
     @staticmethod
     def email_verifier(user):
         User.objects.filter(username = user.username).update(verify_code = create_code())
         user = User.objects.get(username = user.username)
-        sdata = {'body': 'مرحبا بك الدكتور/ة '+ user.first_name + ' ' + user.last_name +' المحترم/ة,\n رمز التفعيل الخاص بك: \n' + user.verify_code,
-        'email': user.email, 'subject': 'Verify your email'}
-        print(sdata['email'])
+        sdata = {
+            'body': 'مرحبا بك الدكتور/ة '+ user.first_name + ' ' + user.last_name +' المحترم/ة,\n رمز التفعيل الخاص بك: \n' + user.verify_code,
+            'email': user.email, 
+            'subject': 'Verify your email'
+        }
         Util.send_email(sdata)
 
 def create_code():
