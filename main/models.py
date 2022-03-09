cda6f3f77c65ed5fbaa5e0eafcfd6e9e25573a1e
thecarrot123 <theknife2001.al@gmail.com>
2022-03-09 22:37:50 +0200

    remove send email

--- main/models.py
+++ main/models.py
@@ -69,7 +69,7 @@ class User(AbstractUser):
     phone = PhoneNumberField("رقم الهاتف",null = True, unique = True)
     points = models.IntegerField("النقاط",default=0,blank = True)
     verify_code = models.CharField(max_length = 10,default='-1',blank=True)
-    verified = models.BooleanField(default=False)
+    verified = models.BooleanField(default=True)
     REQUIRED_FIELDS = ['email','first_name','last_name','points','phone']
     def __str__(self):
         return self.username + ' ' + self.first_name + ' ' + self.last_name
